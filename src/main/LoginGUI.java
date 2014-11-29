27dcc7269d65d7e9895d44d1bc4005fb2ad73842
Angela Porto <portoa@my.erau.edu>
2014-11-29 16:28:40 -0500

    Committing to pull

--- src/main/LoginGUI.java
+++ src/main/LoginGUI.java
@@ -140,12 +140,11 @@ public class LoginGUI {
 							/**
 							* Unleash the Main GUI
 							*/
-<<<<<<< HEAD
+
 							new FXInSwing();
-=======
-							new MainGUI();
-							new UserGUI();
->>>>>>> 7eb4ea783d687b80aa6e782067629c86aede4957
+
+							
+
 	               	}
 						
 						if (confInfoAdmin==true){
