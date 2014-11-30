7ecdc997d20fd0f17054383e8de93af6fc5ac067
Angela Porto <portoa@my.erau.edu>
2014-11-29 22:51:34 -0500

    Method added to AI class to read from file and choose a random item

--- src/main/AI.java
+++ src/main/AI.java
@@ -1,38 +1,63 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+import jxl.Cell;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+
 
 /**
  * @author Proco
  * @version 1.0
  * @created 12-Oct-2014 7:45:57 PM
  */
-public class AI {
+public class AI implements Runnable{
 
-	private int timeInStock;
-	private double timeOrderReady;
-	private double timeSeating;
-	public Middleware m_Middleware;
+	Order order=new Order();
+	private Random randomGenerator;
+	private String[] menuItems;
+	
+	public AI() throws IOException, BiffException{
 
-	public AI(){
+		File inventory=new File("./src/main/Inventory.xls");
+        
+        Workbook inventoryWorkbook1=Workbook.getWorkbook(inventory);
+        WritableWorkbook inventoryWorkbook=Workbook.createWorkbook(new File("./src/main/Inventory.xls"), inventoryWorkbook1);
+        String[] inventorySheetName=inventoryWorkbook.getSheetNames();
+        WritableSheet inventorySheet=inventoryWorkbook.getSheet(inventorySheetName[0]);	
+        
+        int i;
+        for (i=0; i<inventorySheet.getRows(); i++){
+        	
+        	Cell itemName = inventorySheet.getCell(0, i);
+            String menuItem = itemName.getContents();
+        	menuItems[i]=menuItem;
+        	
+        }
+        
+        randomGenerator=new Random();
+	}
+	
 
+	public String chooseRandomItem(){
+		
+		int index=randomGenerator.nextInt(menuItems.length);
+		String item=menuItems[index];
+		return item;
+		
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public void finalize() throws Throwable {
-
-	}
-	public void seatRelease(){
-
-	}
-
-	public void seatTake(){
-
-	}
-
-	public void stockDecrease(){
-
-	}
-
-	public void stockIncrease(){
-
-	}
+	
 }//end AI
